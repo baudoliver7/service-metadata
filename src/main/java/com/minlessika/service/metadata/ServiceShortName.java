e38912466d7bc596650989cbb194ca5daac902f3
Olivier B. OURA <baudoliver7@gmail.com>
2021-12-03 14:32:42 +0000

    create metadata for each service #1

---
+++ src/main/java/com/minlessika/service/metadata/ServiceShortName.java
@@ -0,0 +1,102 @@
+/*
+ * The MIT License (MIT) Copyright (c) 2021 Minlessika
+ * https://github.com/Minlessika/service-metadata/blob/master/LICENSE.txt
+ */
+package com.minlessika.service.metadata;
+
+/**
+ * Shorts names of all services.
+ */
+public enum ServiceShortName {
+
+    /**
+     * Portal service.
+     */
+    MINLESSIKA,
+
+    /**
+     * Software as a Service module.
+     */
+    SAAS,
+
+    /**
+     * Module subscription service.
+     */
+    SUBSCRIPTION,
+
+    /**
+     * API Gateway.
+     */
+    PAYMENT_GATEWAY,
+
+    /**
+     * Membership.
+     * <p>Manages user subscription, accounts, etc.</p>
+     */
+    MEMBERSHIP,
+
+    /**
+     * Logging service.
+     */
+    LOGGING,
+
+    /**
+     * Registry of all services.
+     */
+    SERVICE_REGISTRY,
+
+    /**
+     * Configuration of all services.
+     */
+    SERVICE_CONFIG,
+
+    /**
+     * Supervisor.
+     */
+    SUPERVISOR,
+
+    /**
+     * User account.
+     */
+    ACCOUNT,
+
+    /**
+     * Accounting.
+     */
+    ACCOUNTING,
+
+    /**
+     * Sales.
+     */
+    SALES,
+
+    /**
+     * Point of Sale.
+     */
+    POS,
+
+    /**
+     * Purchases.
+     */
+    PURCHASES,
+
+    /**
+     * Stocks.
+     */
+    STOCKS,
+
+    /**
+     * Electrical energy.
+     */
+    ENERGY,
+
+    /**
+     * Water.
+     */
+    WATER,
+
+    /**
+     * Hotel.
+     */
+    HOTEL
+}
---
+++ src/main/java/com/minlessika/service/metadata/package-info.java
@@ -0,0 +1,10 @@
+/*
+ * The MIT License (MIT) Copyright (c) 2021 Minlessika
+ * https://github.com/Minlessika/service-metadata/blob/master/LICENSE.txt
+ */
+
+/**
+ * Metadata on each service of Minlessika.
+ * @since 0.1
+ */
+package com.minlessika.service.metadata;
